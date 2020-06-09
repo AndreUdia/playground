8456dd225e838ab9ed7fcc93a49173b0ea030bd8
Cabomaldade <soutom@yahoo.com.br>
2020-06-09 13:04:38 -0300

    v2 random hexdecimal color

--- javascript/changebackground/script.js
+++ javascript/changebackground/script.js
@@ -1,11 +1,23 @@
 const colorBtn = document.querySelector('.colorBtn');
 const body = document.querySelector('body');
+const hexValue = document.querySelector('.hexColor');
 
-const colors = ['yellow', 'red', 'green', '#b0f542', '#426cf5', '#1a1d24'];
+const hexColors = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
 colorBtn.addEventListener('click', changeBackgroundColor);
 
 function changeBackgroundColor() {
-    let random = Math.floor(Math.random() * colors.length);
-    body.style.backgroundColor = colors[random];
+    const color = getRandomHex();
+    body.style.backgroundColor = color;
+    hexValue.innerHTML = color;
+    hexValue.style.color = color;
+}
+
+function getRandomHex() {
+    let hexText = '#';
+    for(let i=0; i < 6; i++) {
+        let random = Math.floor(Math.random() * hexColors.length);
+        hexText += hexColors[random];
+    }
+    return hexText;
 }
